8edbdb9ec90be530b4bf7bc1542de82b7b7d0b41
GarbledHamster <jrice@ourstuff.space>
2025-12-29 10:10:19 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "fasting-tracker-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./assets/app-icon-192.png",
+  "./assets/app-icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request);
+    })
+  );
+});
